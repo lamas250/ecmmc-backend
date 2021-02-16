88aaa7e840976bc0631f312cf9a91d206fd43132
Igor <igorlamas1994@gmail.com>
2021-02-16 16:36:10 -0300

    product seeder

--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -6,7 +6,9 @@ import User from '../models/userModel.js';
 const userRouter = express.Router();
 
 userRouter.get('/seed', expressAsyncHandler(async (req, res) => {
+  // await User.remove({});
   const createdUsers = await User.insertMany(data.users);
+  res.send({createdUsers});
 }))
 
 export default userRouter;
--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import data from './data.js';
 import cors from 'cors';
 import mongoose from 'mongoose';
 import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
 app.use(cors())
@@ -16,20 +17,10 @@ app.get('/', (req, res) => {
     res.send('OK');
 })
 
-app.get('/api/products', (req, res) => {
-    res.send(data.products);
-})
+app.use('/api/users', userRouter);
+app.use('/api/products', productRouter);
 
-app.use('/api/users', userRouter)
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find( x => x._id === req.params.id);
-  if(product){
-    res.send(product);
-  }else{
-    res.status(404).send({message: 'Product not Found'})
-  }
-})
-
+// Tratando erro.
 app.use((err, req, res, next) => {
   res.status(500).send({message: err.message});
 })
