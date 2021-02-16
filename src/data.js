8e37fc5d231d35f972734ca561f06fa8efe9e6c7
Igor <igorlamas1994@gmail.com>
2021-02-16 16:12:43 -0300

    user model and seeder

--- src/data.js
+++ src/data.js
@@ -1,4 +1,20 @@
+import bcrypt from 'bcryptjs'
+
 const data = {
+    users: [
+      {
+        name: 'Igor',
+        email: 'igor@teste.com',
+        password: bcrypt.hashSync('123456', 8),
+        isAdmin: true
+      },
+      {
+        name: 'John',
+        email: 'john@teste.com',
+        password: bcrypt.hashSync('123456', 8),
+        isAdmin: false
+      }
+    ],
     products: [
       {
         _id: '1',
--- src/server.js
+++ src/server.js
@@ -1,9 +1,16 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import userRouter from './routers/userRouter.js';
 
 const app = express();
 app.use(cors())
+
+mongoose.connect(
+  'mongodb+srv://igorlamas:igorlamas@cluster0.ijbf1.mongodb.net/ecommerce?retryWrites=true&w=majority',
+  {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true}
+  )
 
 app.get('/', (req, res) => {
     res.send('OK');
@@ -11,6 +18,20 @@ app.get('/', (req, res) => {
 
 app.get('/api/products', (req, res) => {
     res.send(data.products);
+})
+
+app.use('/api/users', userRouter)
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find( x => x._id === req.params.id);
+  if(product){
+    res.send(product);
+  }else{
+    res.status(404).send({message: 'Product not Found'})
+  }
+})
+
+app.use((err, req, res, next) => {
+  res.status(500).send({message: err.message});
 })
 
 
